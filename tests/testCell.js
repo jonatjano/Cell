dff3dc06359a1b56720c0de9f51aa5892b401d14
Jonathan Selle <jselle@greensystemes.com>
2022-04-19 12:39:35 +0200

    using more arrow function in tests

--- tests/testCell.js
+++ tests/testCell.js
@@ -2,23 +2,23 @@ import Cell from "../src/Cell.js"
 
 const expect = chai.expect
 
-describe("cell", function () {
-	it("should exists", function () {
+describe("cell", () => {
+	it("should exists", () => {
 		expect(Cell).not.to.be.undefined
 	})
-	it("should not able to create instance", function () {
+	it("should not able to create instance", () => {
 		expect(() => new Cell()).to.throw(TypeError, "Illegal constructor")
 	})
-	describe("register", function () {
-		it("should be illegal constructor until registered", function () {
+	describe("register", () => {
+		it("should be illegal constructor until registered", () => {
 			class TestRegisterIllegalConstructor extends Cell {}
 			expect(() => {new TestRegisterIllegalConstructor()}).to.throw(TypeError, "Illegal constructor")
 			expect(() => Cell.register(TestRegisterIllegalConstructor)).not.to.throw()
 			expect(() => {new TestRegisterIllegalConstructor()}).not.to.throw()
 		})
 	})
-	describe("extending classes", function () {
-		it("should work", function () {
+	describe("extending classes", () => {
+		it("should work", () => {
 			class TestExtendingWorks extends Cell {}
 			expect(() => Cell.register(TestExtendingWorks)).not.to.throw()
 			let instance = null
@@ -28,8 +28,8 @@ describe("cell", function () {
 			expect(instance).to.be.instanceof(Cell)
 			expect(instance).to.be.instanceof(HTMLElement)
 		})
-		describe("tagname", function () {
-			it("should default to class name", function () {
+		describe("tagname", () => {
+			it("should default to class name", () => {
 				class TestTagnameFromClassName extends Cell {}
 				expect(() => Cell.register(TestTagnameFromClassName)).not.to.throw()
 				let instance = null
@@ -38,7 +38,7 @@ describe("cell", function () {
 				expect(TestTagnameFromClassName.tagName).to.be.equal("test-tagname-from-class-name")
 				expect(instance.tagName).to.be.equal(TestTagnameFromClassName.tagName.toUpperCase())
 			})
-			it("should be personnalisable", function () {
+			it("should be personnalisable", () => {
 				class TestTagnameFromAttribute extends Cell {static tagName = "test-tagname-from-attribute"}
 				expect(() => Cell.register(TestTagnameFromAttribute)).not.to.throw()
 				let instance = null
@@ -47,7 +47,7 @@ describe("cell", function () {
 				expect(TestTagnameFromAttribute.tagName).to.be.equal("test-tagname-from-attribute")
 				expect(instance.tagName).to.be.equal(TestTagnameFromAttribute.tagName.toUpperCase())
 			})
-			it("should be transformed from Pascal/Camel to skewer case", function () {
+			it("should be transformed from Pascal/Camel to skewer case", () => {
 				class TestTagnameSkewerTranformation extends Cell {static tagName = "TestSkewerTransformation"}
 				expect(() => Cell.register(TestTagnameSkewerTranformation)).not.to.throw()
 				let instance = null
@@ -56,13 +56,13 @@ describe("cell", function () {
 				expect(TestTagnameSkewerTranformation.tagName).to.be.equal("test-skewer-transformation")
 				expect(instance.tagName).to.be.equal(TestTagnameSkewerTranformation.tagName.toUpperCase())
 			})
-			it("should be not writable after register", function () {
+			it("should be not writable after register", () => {
 				class TestTagnameCantChangeAfterRegister extends Cell {static tagName = "test-tagname-cant-change-after-register"}
 				expect(() => Cell.register(TestTagnameCantChangeAfterRegister)).not.to.throw()
 				expect(() => {TestTagnameCantChangeAfterRegister.tagName = "something-else"}).to.throw()
 				expect(TestTagnameCantChangeAfterRegister.tagName).to.equal("test-tagname-cant-change-after-register")
 			})
-			it("should auto prepend \"cell-\" if one word only", function () {
+			it("should auto prepend \"cell-\" if one word only", () => {
 				class TestTagnameAutoPrependCellIfOneWord extends Cell {static tagName = "oneword"}
 				expect(() => Cell.register(TestTagnameAutoPrependCellIfOneWord)).not.to.throw()
 				let instance = null
@@ -72,13 +72,13 @@ describe("cell", function () {
 				expect(instance.tagName).to.be.equal(TestTagnameAutoPrependCellIfOneWord.tagName.toUpperCase())
 			})
 		})
-		describe("stylesheet", function () {
-			it("should default to Cell.stylesheet", function () {
+		describe("stylesheet", () => {
+			it("should default to Cell.stylesheet", () => {
 				class TestStylesheetDefault extends Cell {}
 				expect(() => Cell.register(TestStylesheetDefault)).not.to.throw()
 				expect(TestStylesheetDefault.stylesheet).to.be.equal(Cell.stylesheet)
 			})
-			it("should be personnalisable", function () {
+			it("should be personnalisable", () => {
 				class TestStylesheetFromAttribute extends Cell {static stylesheet = "body {background: red;}"}
 				expect(() => Cell.register(TestStylesheetFromAttribute)).not.to.throw()
 				let instance = null
@@ -90,7 +90,7 @@ describe("cell", function () {
 				expect(TestStylesheetFromAttribute.stylesheet.rules[0].style.color).to.be.equal("red")
 			})
 		})
-		describe("template", function () {
+		describe("template", () => {
 		})
 	})
 })
